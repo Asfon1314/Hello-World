92a15bfb867535160d6efcac6eccc807992e3372
Asfon <Asfon1314@gmail.com>
2014-04-12 22:22:56 +0800

    another file

---
+++ MemFuncPoint.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+
+class Foo
+{
+	public:
+		int TT(int a)
+		{
+			cout<<"a is:"<<a<<endl;
+			return 0;
+		}
+};
+
+
+int main()
+{
+	Foo re;
+	auto f= mem_fn(&Foo::TT,&re);
+	f();
+	return 0;
+}
